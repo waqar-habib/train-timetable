9d40d1fe43d24f4e7d0cb701e63e133d8528aabc
Waqar Habib <waqarhabib94@aggienetwork.com>
2019-04-09 23:11:33 -0500

    initial html design

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCO2uw3bJReojZ-mJ1tRM3A8z4USYbtPMw",
+    authDomain: "unit-7-train-scheduler.firebaseapp.com",
+    databaseURL: "https://unit-7-train-scheduler.firebaseio.com",
+    projectId: "unit-7-train-scheduler",
+    storageBucket: "",
+    messagingSenderId: "419160630420"
+};
+firebase.initializeApp(config);
+
